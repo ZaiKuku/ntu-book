d81c249c31328d42f4bc974477d92a1d02bf3c4f
Toshiki Ogimoto <b10902091@csie.ntu.edu.tw>
2023-12-03 17:33:10 +0800

    feat(usedbook-API-endpoint): add routes and controllers for usedbook API endpoint
    BREAKING CHANGE:

--- src/backend/routes/usedBook.js
+++ src/backend/routes/usedBook.js
@@ -1,15 +1,33 @@
-// import {
-//   [handlers]
-// } from "../controllers/usedBook.js";
+import {
+  getUsedBook,
+  getComments,
+  addUsedBook,
+  addComment,
+  updateUsedBook,
+  deleteUsedBook,
+} from "../controllers/usedBook.js";
 import express from "express";
 
-// Create an express router
 const router = express.Router();
 
-// Every path we define here will get /api/usedbook prefix as defined in index.js
+// "/" = "/api/usedbook"
 
-// POST /api/usedbook
-// router.post("/", [handler]); // Here "/" translates to /api/usedbook
+// GET /api/usedbook/:id - Get used book details
+router.get("/:id", getUsedBook);
 
-// export the router
+// GET /api/usedbook/:id/comment - Get all comments for used book
+router.get("/:id/comment", getComments);
+
+// POST /api/usedbook/ - Add used book
+router.post("/", addUsedBook);
+
+// POST /api/usedbook/:id/comment - Add comment to used book
+router.post("/:id/comment", addComment);
+
+// PUT /api/usedbook/:id - Update used book details
+router.put("/:id", updateUsedBook);
+
+// POST /api/usedbook/:id - Delete used book
+router.delete("/:id", deleteUsedBook);
+
 export default router;
